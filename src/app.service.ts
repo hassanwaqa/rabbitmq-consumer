bbd422b2d7a1c6d8e5b08a4ea304d71c3cf205ad
Hassan Waqar <hassanwaqar@ITs-MacBook-Pro.local>
2025-03-27 12:08:25 +0500

    Message Changed

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   handleMessageReceived(text: string) {
-    console.log(`Received a new order - customer: ${text}`);
+    console.log(`New User Created: ${text}`);
   }
 }
